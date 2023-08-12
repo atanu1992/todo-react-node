4241821f15e1d6f92c67591b815b3ad20d7e8867
Atanu Samanta <atanusam1992@gmail.com>
2023-08-12 22:10:34 +0530

    udpate

--- todo-frontend/src/components/pages/after-login/Todo/AddTodo.jsx
+++ todo-frontend/src/components/pages/after-login/Todo/AddTodo.jsx
@@ -16,7 +16,7 @@ const AddTodo = () => {
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
-            for="username"
+            htmlFor="username"
           >
             Username
           </label>
